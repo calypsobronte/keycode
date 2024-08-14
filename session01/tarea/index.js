fdaad4309213808cc301380d7605120f59a96316
Lina María Montaño Ramírez - calypsobronte <lina.montano@mercadolibre.com.co>
2024-08-14 18:24:54 -0500

    Se agrega la primera sesion del curso de Frondent React

--- session01/tarea/index.js
+++ session01/tarea/index.js
@@ -3,5 +3,5 @@ for (let i = 1; i < 11; i++) {
     for (let j = 1; j < 11; j++) {
         console.log(`${i} x ${j} = ${i * j}`);
     }
-    console.log(`------------------`);
+    console.log(`===============`);
 }
